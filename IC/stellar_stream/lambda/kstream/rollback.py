4158f16092c7147fcf4f90945108cfcb6a135a0c
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-09-12 01:40:35 +0530

    aaa

--- IC/stellar_stream/lambda/kstream/rollback.py
+++ IC/stellar_stream/lambda/kstream/rollback.py
@@ -8,7 +8,7 @@ import json
 from enum import Enum
 import boto3
 # import os
-from IC.stack_params import PARAMS
+from stack_params import PARAMS
 
 env_name = 'DEV'  # os.environ['ENV']
 region = PARAMS[env_name]['region']
