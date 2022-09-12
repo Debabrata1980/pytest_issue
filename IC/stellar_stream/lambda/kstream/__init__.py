8b47cf7e8a6b0b189c1f56d0e558c08f55216d58
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-09-12 17:43:27 +0530

    Changes

--- IC/stellar_stream/lambda/kstream/__init__.py
+++ IC/stellar_stream/lambda/kstream/__init__.py
@@ -2,6 +2,6 @@ import os
 import sys
 project_path = os.getcwd()
 src_path = os.path.join(
-    project_path, "IC"
+    project_path, "\\IC\\stellar_stream\\lambda\\kstream"
 )
 sys.path.append(src_path)
